4cfcfdace38497a11c33d7e21b5c482c53b2bf76
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-10-21 13:34:32 +0500

    For Homtask

---
+++ Exam/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        private enum Level
+        {
+            Low = 15,
+            Medium,
+            High = 51
+        }
+        static void Main(string[] args)
+        {
+            Level myVar = Level.Medium;
+            //Console.WriteLine(Level[16]);
+        }
+    }
+}
